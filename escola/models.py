d1e54316e06e5fa6b1b99d78802a7158fd8cbae1
Fernando Freitas <fernandofreitassud2016@gmail.com>
2025-10-24 17:28:44 -0300

    Explicação das views

--- escola/models.py
+++ escola/models.py
@@ -1,1 +1,30 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+
+class Turma(models.Model):
+    id_turma = models.AutoField(primary_key=True)
+    nome_turma = models.CharField(max_length=45)
+
+    class Meta:
+        managed = False
+        db_table = 'turma'
+
+class Aluno(models.Model):
+    id_aluno = models.AutoField(primary_key=True)
+    nome_aluno = models.CharField(max_length=45)
+    datanasc_aluno = models.DateField(db_column='dataNasc_aluno')  # Field name made lowercase.
+    cpf_aluno = models.CharField(max_length=11)
+    obs_aluno = models.TextField(blank=True, null=True)
+    id_turma = models.ForeignKey(Turma, on_delete=models.CASCADE, db_column="id_turma")
+
+    class Meta:
+        managed = False
+        db_table = 'aluno'
+
+
--- setup/settings.py
+++ setup/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crud_escola',
+        'USER': 'fernando',
+        'PASSWORD': 'Freitas@057858',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
